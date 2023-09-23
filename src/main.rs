b862832b66adb0dfcec000b046620f7f3eacb42c
Sparrow0hawk <alexjcoleman@gmail.com>
2023-09-23 14:57:22 +0100

    Add Dockerfile and fly.io deployment config

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use actix_web::{web, App, HttpServer};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
