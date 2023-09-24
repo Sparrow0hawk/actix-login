a08b1c1f779d09af297805b37087a68f2e3fad06
Sparrow0hawk <alexjcoleman@gmail.com>
2023-09-24 14:17:38 +0100

    Add protocol to address

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     let address = "127.0.0.1:8080".to_string();
-    println!("App listening on {}", &address);
+    println!("App listening on http://{}", &address);
 
     let listener = TcpListener::bind(address)?;
 
