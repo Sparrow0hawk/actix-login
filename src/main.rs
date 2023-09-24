21fba275ae58584113ab6c10b3628c4198a8a878
Sparrow0hawk <alexjcoleman@gmail.com>
2023-09-24 08:11:38 +0100

    Add env_logger as described in docs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use actix_login::routes::index;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
