adb58a10c07e6be0a9ed961e83e1d92963b748da
Sparrow0hawk <alexjcoleman@gmail.com>
2023-10-07 10:40:35 +0100

    Add a test for login endpoint

---
+++ tests/test_login.rs
@@ -0,0 +1,22 @@
+mod fixtures;
+
+use fixtures::{get_page_element, spawn_app};
+
+#[actix_web::test]
+async fn test_login() {
+    let app = spawn_app().await;
+
+    let client = reqwest::Client::new();
+
+    let resp = client
+        .get(&format!("{}/login", &app.address))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    let page_str = &resp.text().await.unwrap();
+
+    let page = get_page_element(page_str, "h1");
+
+    assert_eq!(page, "Log In")
+}
